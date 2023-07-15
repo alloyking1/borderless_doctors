ef531e9154fda764895329f00acaf30b26ae7bec
alloyking1 <alloyking1@gmail.com>
2023-07-15 23:46:49 +0100

    added dashboard feature

--- resources/views/components/lander/section-a.blade.php
+++ resources/views/components/lander/section-a.blade.php
@@ -2,24 +2,6 @@
     'show_btn' => false,
 ])
 
-{{-- <div class="bg-gray-600 text-white p-16">
-    <div class="grid grid-cols-1">
-        <div class="md:py-40">
-            <div class="">
-                <h1 class="text-2xl md:text-5xl font-bold">
-                   {{$slot}}
-                </h1>
-            </div>
-            @if ($show_btn)
-            <div class="mt-12">
-                <a href="#" class="border-4 border-red text-white p-6">Click here</a>
-            </div>
-            @endif
-            
-        </div>
-    </div>
-    
-</div> --}}
 <div class="w-full h-full relative bg-gradient-to-r from-sky-800 to-indigo-500">
     <img class="w-full h-full object-cover absolute mix-blend-overlay" src="https://images.pexels.com/photos/3376790/pexels-photo-3376790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">
     <div class="p-24 md:p-56 grid grid-cols-1 text-white">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="text-base text-gray-500 text-thin leading-tight">
+            Hello {{ Auth::user()->name}}
         </h2>
     </x-slot>
 
@@ -9,9 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Doctors Near You") }}
-                   <livewire:doctors-livewire-controller/>
+                   <div class="text-center p-4 mt-4">
+                    <h2 class="text-5xl font-black text-gray-700">What would you love to do today ?</h2>
+                    <div class="px-8 pt-4">
+                        <p class="text-sm text-gray-400 md:px-16">kdflskdf sdkfsld fskfj sdlfksjdfl sdkfsldf ksl ;sldf sdf;sf s;dfs 
+                            sfkmsdfskdfsdfskfs;dkflsjlfksjdfsd
+                            fsdlfjsdlkfjlsdfsdf
+                            ;ksdlf sd;klfsd; fsd;fsdf ;s s;dfs dflks dfls;df s;flsk</p>
+                    </div>
+                   </div>
+                   {{-- <livewire:doctors-livewire-controller/> --}}
+                   <div class="grid md:grid-cols-2 grid-cols-1 gap-4 py-16 px-8">
+                        <a href="#" class="font-black text-2xl text-center rounded-md bg-gradient-to-r from-sky-500 to-blue-500 text-white p-6">Become a doctor</a>
+                    
+                        <a href="#" class="font-black text-2xl text-center bg-gradient-to-r from-blue-500 to-sky-500 text-white rounded-md p-6">Request a doctor</a>
+                    
                 </div>
+                </div>
+               
             </div>
         </div>
     </div>
