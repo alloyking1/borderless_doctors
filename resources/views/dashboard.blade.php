f899cea6729f68af296af18720010bbabd76942a
alloyking1 <alloyking1@gmail.com>
2023-07-20 10:28:22 +0100

    final fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,19 +10,18 @@
             <div class="bg-white overflow-hidden shadow-sm rounded-lg">
                 <div class="p-6 text-gray-900">
                    <div class="text-center p-4 mt-4">
-                    <h2 class="text-5xl font-black text-gray-700">What would you love to do today ?</h2>
+                    <h2 class="text-5xl font-black text-gray-700">Welcome back {{ Auth::user()->name }}</h2>
                     <div class="px-8 pt-4">
-                        <p class="text-sm text-gray-400 md:px-16">kdflskdf sdkfsld fskfj sdlfksjdfl sdkfsldf ksl ;sldf sdf;sf s;dfs 
-                            sfkmsdfskdfsdfskfs;dkflsjlfksjdfsd
-                            fsdlfjsdlkfjlsdfsdf
-                            ;ksdlf sd;klfsd; fsd;fsdf ;s s;dfs dflks dfls;df s;flsk</p>
+                        <p class="text-sm text-gray-400 md:px-16">
+                            Click the button below to start your leave on request application process
+                        </p>
                     </div>
                    </div>
                    {{-- <livewire:doctors-livewire-controller/> --}}
-                   <div class="grid md:grid-cols-2 grid-cols-1 gap-4 py-16 px-8">
-                        <a href="{{ route('dashboard.become-a-doctor') }}" class="font-black text-2xl text-center rounded-md bg-gradient-to-r from-sky-500 to-blue-500 text-white p-6">Become a doctor</a>
+                   <div class="flex justify-center py-16 px-8">
+                        {{-- <a href="{{ route('dashboard.become-a-doctor') }}" class="font-black text-2xl text-center rounded-md bg-gradient-to-r from-sky-500 to-blue-500 text-white p-6">Become a doctor</a> --}}
                     
-                <a href="{{ route('dashboard.request-a-doctor') }}" class="font-black text-2xl text-center bg-gradient-to-r from-blue-500 to-sky-500 text-white rounded-md p-6">Request a doctor</a>
+                <a href="{{ route('dashboard.request-a-doctor') }}" class="font-black text-2xl text-center bg-gradient-to-r from-blue-500 to-sky-500 text-white rounded-md p-6">Request for emergency leave of your loved ones</a>
                     
                 </div>
                 </div>
